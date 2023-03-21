3ca78c1e164d27f2145ab65b81fb28c61789c289
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-03-20 21:40:58 -0400

    yarn.mk: more commands

--- yarn.mk
+++ yarn.mk
@@ -6,20 +6,29 @@ install:## 	$(PACKAGE_MANAGER) install
 	@$(PACKAGE_MANAGER) install
 lint:## 	eslint --ext .ts,.tsx .
 	@$(PACKAGE_MANAGER) run lint
+make:electron-forge-make## 	electron-forge-make
 electron-forge-make:## 	electron-forge make
+	@rm -rf out/make-nvm-darwin-x64
 	@$(PACKAGE_MANAGER) run make
+package:electron-forge-package## 	electron-forge-package
 electron-forge-package:## 	electron-forge package
 	@$(PACKAGE_MANAGER) run package
+publish:electron-forge-publish## 	electron-forge-publish
 electron-forge-publish:## 	electron-forge publish
 	@$(PACKAGE_MANAGER) run publish
+start:electron-forge-publish## 	electron-forge-start
 electron-forge-start:## 	electron-forge start
 	@$(PACKAGE_MANAGER) run start
+cli:electron-forge-cli## 	electron-forge-cli
 electron-forge-cli:## 	yarn add --dev @electron-forge/cli
 	@yarn add --dev @electron-forge/cli
+import:electron-forge-import## 	electron-forge-import
 electron-forge-import:## 	yarn electron-forge import
 	@yarn electron-forge import
+app-wp:electron-app-wp## 	electron-app-wp
 electron-app-wp:## 	template-webpack
 	@yarn create electron-app $(GIT_BRANCH)-wp --template=webpack
+app-wp-ts:electron-app-wp-ts## 	electron-app-wp-ts
 electron-app-wp-ts:## 	template-webpack-typescript
 	@yarn create electron-app $(GIT_BRANCH)-wp-ts --template=webpack-typescript
 # vim: set noexpandtab:
